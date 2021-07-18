12f9358d9e64c457d5e84968118f1b58b9492192
Pilar Cortez <73955741+pilaricp@users.noreply.github.com>
2021-07-17 23:50:12 -0400

    Update enviar-prueba.php

--- enviar-prueba.php
+++ enviar-prueba.php
@@ -21,8 +21,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'programasemestrales@gmail.com';                     //SMTP username
-    $mail->Password   = 'Contrasena21';                               //SMTP password
+    $mail->Username   = ' ';                     //SMTP username
+    $mail->Password   = ' ';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
